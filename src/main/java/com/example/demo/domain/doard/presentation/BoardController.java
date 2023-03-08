7b97ee05ddeb0676ad96c8451a981a46c38b1baf
cutehanchankyo <jeongchangyo71@gmail.com>
2023-03-08 22:39:28 +0900

    add PostController

--- src/main/java/com/example/demo/domain/doard/presentation/BoardController.java
+++ src/main/java/com/example/demo/domain/doard/presentation/BoardController.java
@@ -1,10 +1,12 @@
 package com.example.demo.domain.doard.presentation;
 
+import com.example.demo.domain.doard.presentation.dto.request.BoardRequest;
 import com.example.demo.domain.doard.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/board")
@@ -14,5 +16,10 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> create(@RequestBody BoardRequest boardRequest){
+        boardService.create(boardRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
