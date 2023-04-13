9d8d930210f64059748a1b2e16450352c2153102
jeong <jeongchangyo71@gmail.com>
2023-04-13 20:43:25 +0900

    :memo: EmailSendRequest 추가

---
+++ src/main/java/com/example/demo/domain/email/presentation/dto/EmailSendRequest.java
@@ -0,0 +1,17 @@
+package com.example.demo.domain.email.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailSendRequest {
+    @Email
+    @NotBlank(message = "이메일은 필수 입력값입니다")
+    private String email;
+}
