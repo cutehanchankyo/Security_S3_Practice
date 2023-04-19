a18a3c4a3adc4a591eb97eb57754ba18efd54f7c
jeong <jeongchangyo71@gmail.com>
2023-04-19 16:45:40 +0900

    commit

---
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.domain.user.entity;
+
+import com.example.demo.domain.user.enuns.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
