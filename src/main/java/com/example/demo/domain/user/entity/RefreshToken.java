3abe0f02e89b1b2c9cde6f174979a7167efcfbe9
jeong <jeongchangyo71@gmail.com>
2023-04-19 16:45:33 +0900

    commit

---
+++ src/main/java/com/example/demo/domain/user/entity/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.stereotype.Indexed;
+
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@RedisHash(value = "refeshToken")
+public class RefreshToken {
+    @Indexed
+    private String email;
+
+    @Id
+    private String token;
+
+    @TimeToLive
+    private Long expiredAt;
+
+    public void exchangeRefreshToken(String token) {
+        this.token = token;
+    }
+}
