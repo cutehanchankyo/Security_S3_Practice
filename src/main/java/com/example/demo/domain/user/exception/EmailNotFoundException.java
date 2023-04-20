c9c5cfb006cc9c5f1e0835ddeaf92fa6251a8dd7
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:37 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/EmailNotFoundException.java
+++ src/main/java/com/example/demo/domain/user/exception/EmailNotFoundException.java
@@ -1,4 +1,16 @@
 package com.example.demo.domain.user.exception;
 
-public class EmailNotFoundException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class EmailNotFoundException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public EmailNotFoundException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.EMAIL_NOT_FOUND;
+    }
 }
