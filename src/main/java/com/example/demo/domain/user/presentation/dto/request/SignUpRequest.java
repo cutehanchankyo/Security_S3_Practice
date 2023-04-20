6ad2a1f4450ca846910573a7af4c26e8d91c5957
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:57 +0900

    commit

---
+++ src/main/java/com/example/demo/domain/user/presentation/dto/request/SignUpRequest.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain.user.presentation.dto.request;
+
+import com.example.demo.domain.user.enums.Role;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+import javax.validation.constraints.*;
+
+@Getter
+@RequiredArgsConstructor
+public class SignUpRequest {
+
+    @Email
+    @NotBlank(message = "이메일은 필수 입력값입니다")
+    private final String email;
+    @NotBlank(message = "이름은 필수 입력값입니다")
+    private final String name;
+    @NotBlank(message = "비밀번호는 필수 입력값입니다")
+    private final String password;
+    private final Role role;
+}
