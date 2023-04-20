85956405753e6f58b142a5221b4da2e07515b241
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:45 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/RefreshTokenNotFoundException.java
+++ src/main/java/com/example/demo/domain/user/exception/RefreshTokenNotFoundException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.user.exception;
 
-public class RefreshTokenNotFoundException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class RefreshTokenNotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public RefreshTokenNotFoundException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.REFRESH_TOKEN_NOT_FOUND;
+    }
 }
