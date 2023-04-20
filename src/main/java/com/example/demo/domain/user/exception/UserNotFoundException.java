79a4369cc1c13fd79ba9c4082af951ee20c77bfe
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:14:03 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/example/demo/domain/user/exception/UserNotFoundException.java
@@ -1,4 +1,15 @@
 package com.example.demo.domain.user.exception;
 
-public class UserNotFoundException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class UserNotFoundException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public UserNotFoundException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.USER_NOT_FOUND;
+    }
 }
+
