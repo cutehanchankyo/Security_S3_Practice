b96dfe1c61fbbb011ab41084eaa10fed062a6b84
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:14:10 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/WrongPasswordException.java
+++ src/main/java/com/example/demo/domain/user/exception/WrongPasswordException.java
@@ -1,4 +1,16 @@
 package com.example.demo.domain.user.exception;
 
-public class WrongPasswordException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class WrongPasswordException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public WrongPasswordException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.WRONG_PASSWORD;
+    }
 }
