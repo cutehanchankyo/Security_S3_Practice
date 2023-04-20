31ac4a58c856fb20c1eec45e737f7a96064eb5d7
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:14:06 +0900

    commit

---
+++ src/main/java/com/example/demo/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.user.repository;
+
+import com.example.demo.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Boolean existsByEmail(String email);
+    Optional<User> findUserByEmail(String email);
+}
