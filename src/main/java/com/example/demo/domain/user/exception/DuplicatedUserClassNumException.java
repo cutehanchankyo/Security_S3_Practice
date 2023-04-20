b6fdae27e60067bcbbe1c00fa1b800e7b7ff5dbc
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:25 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/DuplicatedUserClassNumException.java
+++ src/main/java/com/example/demo/domain/user/exception/DuplicatedUserClassNumException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.user.exception;
 
-public class DuplicatedUserClassNumException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class DuplicatedUserClassNumException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public DuplicatedUserClassNumException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.CLASS_NUMBER_ALREADY_EXIST;
+    }
 }
