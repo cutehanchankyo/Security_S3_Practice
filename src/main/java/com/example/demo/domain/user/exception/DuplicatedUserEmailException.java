fc107616b528b56785e023e1d1c3024c9054f59c
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:34 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/DuplicatedUserEmailException.java
+++ src/main/java/com/example/demo/domain/user/exception/DuplicatedUserEmailException.java
@@ -1,4 +1,16 @@
 package com.example.demo.domain.user.exception;
 
-public class DuplicatedUserEmailException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DuplicatedUserEmailException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public DuplicatedUserEmailException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.EMAIL_ALREADY_EXIST;
+    }
 }
