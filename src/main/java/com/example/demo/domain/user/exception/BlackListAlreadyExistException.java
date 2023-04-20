e2be8e3551b4a715961c2362f62c22f26c515bc7
jeong <jeongchangyo71@gmail.com>
2023-04-20 20:13:20 +0900

    commit

--- src/main/java/com/example/demo/domain/user/exception/BlackListAlreadyExistException.java
+++ src/main/java/com/example/demo/domain/user/exception/BlackListAlreadyExistException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.user.exception;
 
-public class BlackListAlreadyExistException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BlackListAlreadyExistException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BlackListAlreadyExistException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.BLACK_LIST_ALREADY_EXIST;
+    }
 }
