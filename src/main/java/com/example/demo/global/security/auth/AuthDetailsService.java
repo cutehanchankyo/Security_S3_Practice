64f605d1f7ba015e1a61b58a6e0d6c524505ac01
jeong <jeongchangyo71@gmail.com>
2023-04-21 09:00:56 +0900

    commit

---
+++ src/main/java/com/example/demo/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,46 @@
+package com.example.demo.global.security.auth;
+
+import com.example.demo.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
+}
