e8498f32a0a2b92f9184821349b1612c4ccdb24c
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:40 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/service/CreateCommentService.java
+++ src/main/java/com/example/demo/domain/comment/service/CreateCommentService.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.comment.service;
 
+import com.example.demo.domain.comment.presentation.dto.request.CreateCommentReqDto;
+
 public interface CreateCommentService {
+    void execute(Long postId, CreateCommentReqDto createCommentReqDto);
 }
