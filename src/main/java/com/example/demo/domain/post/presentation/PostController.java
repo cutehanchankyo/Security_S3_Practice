a1a790c49678247e1e61e8492e8544de0f3a2543
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:25:25 +0900

    commit

--- src/main/java/com/example/demo/domain/post/presentation/PostController.java
+++ src/main/java/com/example/demo/domain/post/presentation/PostController.java
@@ -2,11 +2,9 @@ package com.example.demo.domain.post.presentation;
 
 import com.example.demo.domain.post.presentation.dto.request.CreatePostRequest;
 import com.example.demo.domain.post.presentation.dto.request.ModifyPostRequest;
+import com.example.demo.domain.post.presentation.dto.response.PostDetailResponse;
 import com.example.demo.domain.post.presentation.dto.response.PostListResDto;
-import com.example.demo.domain.post.service.CreatePostService;
-import com.example.demo.domain.post.service.ModifyPostService;
-import com.example.demo.domain.post.service.PostListService;
-import com.example.demo.domain.post.service.RemovePostService;
+import com.example.demo.domain.post.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +21,7 @@ public class PostController {
     private final ModifyPostService modifyPostService;
     private final RemovePostService removePostService;
     private final PostListService postListService;
+    private final OnePostService onePostService;
 
     @PostMapping
     public ResponseEntity<Void> createPost(@RequestBody @Valid CreatePostRequest createPostRequest) {
@@ -46,6 +45,12 @@ public class PostController {
     public ResponseEntity<List<PostListResDto>> findAllPost() {
         List<PostListResDto> results = postListService.execute();
         return new ResponseEntity<>(results, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDetailResponse> findOne(@PathVariable("id") Long id){
+        PostDetailResponse oneFindById = onePostService.execute(id);
+        return new ResponseEntity<>(oneFindById,HttpStatus.OK);
     }
 
 }
