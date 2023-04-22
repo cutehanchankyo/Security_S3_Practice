b06623297995e0e0fe3c1fbd467c6d447a969302
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:07:26 +0900

    commit

--- src/main/java/com/example/demo/domain/post/service/CreatePostService.java
+++ src/main/java/com/example/demo/domain/post/service/CreatePostService.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.post.service;
 
+import com.example.demo.domain.post.presentation.dto.request.CreatePostRequest;
+
 public interface CreatePostService {
+    void execute(CreatePostRequest createPostRequest);
 }
