ab35b852eb4c0ed4cfbf21598d93c0a5ff37bf45
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:38 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/presentation/dto/request/CreateCommentReqDto.java
+++ src/main/java/com/example/demo/domain/comment/presentation/dto/request/CreateCommentReqDto.java
@@ -1,4 +1,15 @@
 package com.example.demo.domain.comment.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCommentReqDto {
+    @NotBlank(message = "댓글은 공백을 허용하지 않습니다")
+    private String content;
 }
