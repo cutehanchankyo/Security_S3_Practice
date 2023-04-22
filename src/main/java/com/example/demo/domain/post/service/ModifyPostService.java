6811c2cfd1737ebbcc2c9410ac8d8115c61e976c
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:11:22 +0900

    commit

--- src/main/java/com/example/demo/domain/post/service/ModifyPostService.java
+++ src/main/java/com/example/demo/domain/post/service/ModifyPostService.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.post.service;
 
+import com.example.demo.domain.post.presentation.dto.request.ModifyPostRequest;
+
 public interface ModifyPostService {
+    void execute(Long id, ModifyPostRequest modifyPostRequest);
 }
