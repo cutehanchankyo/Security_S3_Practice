5e9c6d8992b6cea8996865a05bfccba6be7c26a4
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:11:33 +0900

    commit

--- src/main/java/com/example/demo/domain/post/service/PostListService.java
+++ src/main/java/com/example/demo/domain/post/service/PostListService.java
@@ -1,4 +1,9 @@
 package com.example.demo.domain.post.service;
 
+import com.example.demo.domain.post.presentation.dto.response.PostListResDto;
+
+import java.util.List;
+
 public interface PostListService {
+    List<PostListResDto> execute();
 }
