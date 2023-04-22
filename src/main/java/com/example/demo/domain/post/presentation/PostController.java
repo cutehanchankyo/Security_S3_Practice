d6b72326ee6526d6591ea241bb250c9588a15289
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:07:28 +0900

    commit

--- src/main/java/com/example/demo/domain/post/presentation/PostController.java
+++ src/main/java/com/example/demo/domain/post/presentation/PostController.java
@@ -1,4 +1,30 @@
 package com.example.demo.domain.post.presentation;
 
+import com.example.demo.domain.post.presentation.dto.request.CreatePostRequest;
+import com.example.demo.domain.post.service.CreatePostService;
+import com.example.demo.domain.post.service.ModifyPostService;
+import com.example.demo.domain.post.service.PostListService;
+import com.example.demo.domain.post.service.RemovePostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+    private final CreatePostService createPostService;
+    private final ModifyPostService modifyPostService;
+    private final RemovePostService removePostService;
+    private final PostListService postListService;
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody @Valid CreatePostRequest createPostRequest) {
+        createPostService.execute(createPostRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
