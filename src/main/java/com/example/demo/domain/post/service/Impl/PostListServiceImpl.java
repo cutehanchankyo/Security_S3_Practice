3c1ff6b7942d3df473ba5ece2c6e5d80a90d1efa
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:11:26 +0900

    commit

--- src/main/java/com/example/demo/domain/post/service/Impl/PostListServiceImpl.java
+++ src/main/java/com/example/demo/domain/post/service/Impl/PostListServiceImpl.java
@@ -1,4 +1,30 @@
 package com.example.demo.domain.post.service.Impl;
 
-public class PostListServiceImpl {
+import com.example.demo.domain.post.presentation.dto.response.PostListResDto;
+import com.example.demo.domain.post.repository.PostRepository;
+import com.example.demo.domain.post.service.PostListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostListServiceImpl implements PostListService {
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<PostListResDto> execute() {
+        return postRepository.findAll().stream()
+                .map(post -> new PostListResDto(
+                        post.getPostId(),
+                        post.getTitle(),
+                        post.getUser().getEmail(),
+                        post.getUser().getName()
+                )).collect(Collectors.toList());
+    }
 }
