71569c256a9d7f65cffc0a3aad7a0f5357a7c245
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:50 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/presentation/CommentCotronller.java
+++ src/main/java/com/example/demo/domain/comment/presentation/CommentCotronller.java
@@ -1,4 +1,27 @@
 package com.example.demo.domain.comment.presentation;
 
-public class CommentCotronller {
+import com.example.demo.domain.comment.presentation.dto.request.CreateCommentReqDto;
+import com.example.demo.domain.comment.service.CreateCommentService;
+import com.example.demo.domain.comment.service.DeleteCommentService;
+import com.example.demo.domain.comment.service.ModifyCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+    private final CreateCommentService createCommentService;
+    private final ModifyCommentService modifyCommentService;
+    private final DeleteCommentService deleteCommentService;
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<Void> createComment(@PathVariable Long postId, @RequestBody @Valid CreateCommentReqDto createCommentReqDto) {
+        createCommentService.execute(postId, createCommentReqDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
