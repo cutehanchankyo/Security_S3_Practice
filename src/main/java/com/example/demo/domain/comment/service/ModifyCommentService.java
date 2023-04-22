bf595a3affed134755f87df61e366feaadb30279
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:45:41 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/service/ModifyCommentService.java
+++ src/main/java/com/example/demo/domain/comment/service/ModifyCommentService.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.comment.service;
 
+import com.example.demo.domain.comment.presentation.dto.request.ModifyCommentReqDto;
+
 public interface ModifyCommentService {
+    void execute(Long commentId, ModifyCommentReqDto modifyCommentReqDto);
 }
