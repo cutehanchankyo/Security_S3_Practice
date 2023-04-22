040f5cf3e3ff00c91f878cea7510c1c78565e287
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:45 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/exception/CommentNotFoundException.java
+++ src/main/java/com/example/demo/domain/comment/exception/CommentNotFoundException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.comment.exception;
 
-public class CommentNotFoundException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CommentNotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CommentNotFoundException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.EMAIL_NOT_FOUND;
+    }
 }
