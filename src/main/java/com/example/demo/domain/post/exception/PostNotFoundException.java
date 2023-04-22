d672d4322a94cfce118ddb2586060fde483572eb
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:03:10 +0900

    commit

--- src/main/java/com/example/demo/domain/post/exception/PostNotFoundException.java
+++ src/main/java/com/example/demo/domain/post/exception/PostNotFoundException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.post.exception;
 
-public class PostNotFoundException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PostNotFoundException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public PostNotFoundException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.POST_NOT_FOUND;
+    }
 }
