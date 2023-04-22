0627c6b8cb1a869a5575a8e5b1c562029ed672de
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:45:46 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/presentation/dto/request/ModifyCommentReqDto.java
+++ src/main/java/com/example/demo/domain/comment/presentation/dto/request/ModifyCommentReqDto.java
@@ -1,4 +1,15 @@
 package com.example.demo.domain.comment.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ModifyCommentReqDto {
+    @NotBlank(message = "수정된 댓글은 공백일 수 없습니다")
+    private String content;
 }
