f2378061fc4e3f875718be586ccdd72e49ff775b
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:48 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/exception/CommentUserMismatchException.java
+++ src/main/java/com/example/demo/domain/comment/exception/CommentUserMismatchException.java
@@ -1,4 +1,14 @@
 package com.example.demo.domain.comment.exception;
 
-public class CommentUserMismatchException {
+import com.example.demo.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CommentUserMismatchException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public CommentUserMismatchException(String message) {
+        super(message);
+        this.errorCode = ErrorCode.COMMENT_USER_MISMATCH;
+    }
 }
