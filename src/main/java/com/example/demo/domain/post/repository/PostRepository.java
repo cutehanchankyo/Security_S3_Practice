9249d23883fad1ba1bf28db9f193af177e4f03c8
jeong <jeongchangyo71@gmail.com>
2023-04-22 13:21:26 +0900

    commit

--- src/main/java/com/example/demo/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/demo/domain/post/repository/PostRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.post.repository;
 
-public interface PostRepository {
+import com.example.demo.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
