51ae31d5e0915f76cae50f0c4f5e87b7e0a6866e
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:05:41 +0900

    commit

--- src/main/java/com/example/demo/domain/post/presentation/dto/request/CreatePostRequest.java
+++ src/main/java/com/example/demo/domain/post/presentation/dto/request/CreatePostRequest.java
@@ -1,4 +1,19 @@
 package com.example.demo.domain.post.presentation.dto.request;
 
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class CreatePostRequest {
+    @NotBlank(message = "제목은 필수 입력값입니다")
+    private final String title;
+    @NotBlank(message = "내용은 필수 입력값입니다")
+    private final String content;
 }
