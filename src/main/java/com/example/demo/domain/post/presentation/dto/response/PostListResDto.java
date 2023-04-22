21472957d53d444fc64c215f7722476e77cb9dcc
jeong <jeongchangyo71@gmail.com>
2023-04-22 14:05:43 +0900

    commit

---
+++ src/main/java/com/example/demo/domain/post/presentation/dto/response/PostListResDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.post.presentation.dto.response;
+
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostListResDto {
+    private Long postId;
+    private String title;
+    private String email;
+    private String name;
+}
