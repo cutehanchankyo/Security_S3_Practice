27a1240dda1460a4fc76fc7d395165a0076a4c10
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:35 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/entity/Comment.java
+++ src/main/java/com/example/demo/domain/comment/entity/Comment.java
@@ -1,4 +1,44 @@
 package com.example.demo.domain.comment.entity;
 
+import com.example.demo.domain.post.entity.Post;
+import com.example.demo.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+
+    public Comment update(Long id, String content) {
+        Comment comment = Comment.builder()
+                .content(content)
+                .post(this.post)
+                .user(this.user)
+                .build();
+        comment.id = id;
+        return comment;
+    }
 }
