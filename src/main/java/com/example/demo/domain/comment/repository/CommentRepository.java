9d7b811e0f645dfec989276284309e68089c7f3f
jeong <jeongchangyo71@gmail.com>
2023-04-23 00:42:36 +0900

    commit

--- src/main/java/com/example/demo/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/demo/domain/comment/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.domain.comment.repository;
 
-public interface CommentRepository {
+import com.example.demo.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
